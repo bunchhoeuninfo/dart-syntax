80da785be56809e3a82888b5d06bdd03a49e5a14
Bunchhoeun.Chhim <bunchhoeun.chhim@gmail.com>
2023-11-23 21:21:43 +0700

    compute Pi

--- bin/basic_syntax.dart
+++ bin/basic_syntax.dart
@@ -1,5 +1,43 @@
-import 'package:basic_syntax/basic_syntax.dart' as basic_syntax;
+import 'dart:math' show Random;
 
-void main(List<String> arguments) {
-  print('Hello world: yesssssssssssssss');
+void main() async {
+  print('compute II using the Monte Carlo method.');
+  await for (final estimate in computePi().take(100)) {
+    print('pi =~ $estimate');
+
+  }
+}
+
+//Generate a stream of increasingly accurate esitmates of II...
+Stream<double> computePi({int batch = 10000}) async* {
+  var total = 0; //Inferred to be of type int
+  var count = 0;
+
+  while (true) {
+    final points = generateRandom().take(batch);
+    final inside = points.where((p) => p.isInsideUnitCircle);
+
+    total += batch;
+    count += inside.length;
+    final ratio = count / total;
+
+    yield ratio * 4;
+
+  }
+}
+
+Iterable<Point> generateRandom([int? seed]) sync* {
+  final random = Random(seed);
+  while (true) {
+    yield Point(random.nextDouble(), random.nextDouble());
+  }
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  const Point(this.x, this.y);
+
+  bool get isInsideUnitCircle => x * x + y * y <= 1;
 }
