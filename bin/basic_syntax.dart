433e47643e8078eb8931c33b233d13a9eb6707eb
Bunchhoeun.Chhim <bunchhoeun.chhim@gmail.com>
2023-11-23 20:58:53 +0700

    sample

--- bin/basic_syntax.dart
+++ bin/basic_syntax.dart
@@ -1,5 +1,5 @@
 import 'package:basic_syntax/basic_syntax.dart' as basic_syntax;
 
 void main(List<String> arguments) {
-  print('Hello world: ${basic_syntax.calculate()}!');
+  print('Hello world: yesssssssssssssss');
 }
