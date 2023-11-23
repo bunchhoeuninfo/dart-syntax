07bd87482f174379d3ff1aebd2b1df732a1b24c1
Bunchhoeun.Chhim <bunchhoeun.chhim@gmail.com>
2023-11-23 20:55:38 +0700

    main branch

---
+++ bin/basic_syntax.dart
@@ -0,0 +1,5 @@
+import 'package:basic_syntax/basic_syntax.dart' as basic_syntax;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basic_syntax.calculate()}!');
+}
---
+++ test/basic_syntax_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_syntax/basic_syntax.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
